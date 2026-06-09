d3f89920c8ac0f4b2c39506c26ca08989dac47cb
zabekmail-cell <sylwia.zabek.mail@gmail.com>
2026-06-09 09:04:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-import requests  # Added missing import
-from flask import Flask, redirect, Response  # Added missing Response import
+import requests  
+from flask import Flask, redirect, Response  
 
 app = Flask(__name__)
 
@@ -9,13 +9,13 @@ def home():
     return "IPTV Proxy Server is Live!"
 
 # --- CHANNEL 1 ---
-@app.route('/1/myplaylist_06062026.m3u8')
+@app.route('/1/myplaylist_06062026.m3u8') # Route 1
 def proxy_stream1():
     target_url = "http://103.254.122.129/live/290113/24475A/1457769.m3u8?_t=1780952700819&_vnc=mq5p9phk_26&token=SEReU0dfFg8WUQdQBgVWAlNTAVVSBAYKUQdWUVUDBwBRVgRXVwFWA1YWGxRGRUZWVl9tXVAWWAgHVwAEBwpJFkYVU0BrXlAXDhYGAAUCAwoDEB4WQF0PXBFYBx8TQwxQEVwUAQYFDAQWGBVTTUVRQVlTXmtQURRQUAcVCV9GCVgfRFlca1NRWFVaUxQPEwQRGxBbR0QWWFdGDlsfE1EMQEEHQlcWDRYCDAMVGhdSW0ZZRkBNFg5AaX9AGxFUSxFRXRVfXVoVDhdZB0IOFx0WWkRtQFFHQBBcUg9SQRMJRwQRShRfVU9rVltaWVNWRV1cWUEQDhYFQBURD1hdWEcKRmwWX1YWDRYEBQMBBhcdFkZEV0BrXURAAxFVAB0HBksFA1cYAwMHFkg%3D"
     return redirect(target_url, code=302)
 
 # --- CHANNEL 2 ---
-@app.route('/1/myplaylist_06062026.m3u8')
+@app.route('/2/myplaylist_06062026.m3u8') # Route 2 
 def proxy_stream2():
     target_url = "http://line.fire-4k.cc/live/290113/24475A/1457496.m3u8?_lsr=mq47jrig_1"
     return redirect(target_url, code=302)
